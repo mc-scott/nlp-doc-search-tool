d61d36ce3e3bed0b29612b902e0479a79c9b20a9
Matthew Scott <msmattscott@gmail.com>
2024-08-21 17:28:54 +0100

    test

--- utils.py
+++ utils.py
@@ -29,12 +29,10 @@ def fn_get_pdf_text(filepath:str) -> str:
     # assert isinstance(filepath, str), "ERROR: filepath must be a string."
 
     output = ""
-    try:
-        pdf_reader = PdfReader(filepath)
-        for page in pdf_reader.pages:
-            output += page.extract_text()
-    except Exception as e:
-        print(f"ERROR: {e}")
+    pdf_reader = PdfReader(filepath)
+    for page in pdf_reader.pages:
+        output += page.extract_text()
+
 
     return output
 # end
